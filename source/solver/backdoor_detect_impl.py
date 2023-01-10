859c07368658105355ef35070f7f083aef1a82d0
NayMyatMin <naymyatmin97@gmail.com>
2023-01-10 16:46:04 +0000

    Added Resnet18 Model

--- source/solver/backdoor_detect_impl.py
+++ source/solver/backdoor_detect_impl.py
@@ -32,7 +32,276 @@ from nn_utils import *
 import time
 import statistics
 
+###### Resnet - 18
+import torch
+import torch.nn as nn
+import os
 
+
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(
+        in_planes,
+        out_planes,
+        kernel_size=3,
+        stride=stride,
+        padding=dilation,
+        groups=groups,
+        bias=False,
+        dilation=dilation,
+    )
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(
+        self,
+        inplanes,
+        planes,
+        stride=1,
+        downsample=None,
+        groups=1,
+        base_width=64,
+        dilation=1,
+        norm_layer=None,
+    ):
+        super(BasicBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError("BasicBlock only supports groups=1 and base_width=64")
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(
+        self,
+        inplanes,
+        planes,
+        stride=1,
+        downsample=None,
+        groups=1,
+        base_width=64,
+        dilation=1,
+        norm_layer=None,
+    ):
+        super(Bottleneck, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.0)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(
+        self,
+        block,
+        layers,
+        num_classes=10,
+        zero_init_residual=False,
+        groups=1,
+        width_per_group=64,
+        replace_stride_with_dilation=None,
+        norm_layer=None,
+    ):
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError(
+                "replace_stride_with_dilation should be None "
+                "or a 3-element tuple, got {}".format(replace_stride_with_dilation)
+            )
+        self.groups = groups
+        self.base_width = width_per_group
+
+        # CIFAR10: kernel_size 7 -> 3, stride 2 -> 1, padding 3->1
+        self.conv1 = nn.Conv2d(
+            3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False
+        )
+        # END
+
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(
+            block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0]
+        )
+        self.layer3 = self._make_layer(
+            block, 256, layers[2], stride=2, dilate=replace_stride_with_dilation[1]
+        )
+        self.layer4 = self._make_layer(
+            block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2]
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(
+            block(
+                self.inplanes,
+                planes,
+                stride,
+                downsample,
+                self.groups,
+                self.base_width,
+                previous_dilation,
+                norm_layer,
+            )
+        )
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(
+                block(
+                    self.inplanes,
+                    planes,
+                    groups=self.groups,
+                    base_width=self.base_width,
+                    dilation=self.dilation,
+                    norm_layer=norm_layer,
+                )
+            )
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.reshape(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+class SubResNet(nn.Module):
+    def __init__(
+        self,
+        block,
+        num_classes=10,
+    ):
+        super().__init__()
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        
+    def forward(self, x):
+        x = self.avgpool(x)
+        x = x.reshape(x.size(0), -1)
+        x = self.fc(x)
+        return x
+###########################################
 
 class MNISTNet(nn.Module):
     def __init__(self):
@@ -50,6 +319,7 @@ class MNISTNet(nn.Module):
         x = F.relu(x)
         x = self.fc3(x)
         x = F.relu(x)
+        print(f"x1 is: {x.shape}")
         x = self.fc4(x)
         output = x # cross entropy in pytorch already includes softmax
         return output
@@ -142,6 +412,10 @@ class BackdoorDetectImpl():
             elif (dataset == 'cifar10_inv' or dataset == 'cifar10_sem' or dataset == 'cifar10_tro') \
                 and ('fc3.weight' in name or 'fc3.bias' in name):
                 dict_params[name].data.copy_(param.data)
+            elif dataset == 'cifar10' and ('fc.weight' in name or 'fc.bias' in name):
+                dict_params[name].data.copy_(param.data)
+            elif dataset == 'cifar' and ('fc3.weight' in name or 'fc3.bias' in name):
+                dict_params[name].data.copy_(param.data)
         
         sub_model.load_state_dict(dict_params)
 
@@ -151,6 +425,8 @@ class BackdoorDetectImpl():
 
         for epoch in range(num_of_epochs):
             for batch, (x, y) in enumerate(dataloader):
+                # print("Input tensor shape: ", x.shape)
+                # print("Adversarial trigger shape: ", delta.shape)
                 delta.requires_grad = True
                 x_adv = torch.clamp(torch.add(x, delta), minx, maxx)
                 target_tensor = torch.full(y.size(), target)
@@ -162,15 +438,15 @@ class BackdoorDetectImpl():
 
                 grad_data = delta.grad.data
                 delta = torch.clamp(delta - eps * grad_data.sign(), -10.0, 10.0).detach()
-
         return delta
 
 
     def __check(self, model, dataloader, delta, target):
         size = len(dataloader.dataset)
         correct = 0
-
+        
         for batch, (x, y) in enumerate(dataloader):
+
             x_adv = torch.clamp(torch.add(x, delta), 0.0)
             target_tensor = torch.full(y.size(), target)
 
@@ -191,7 +467,7 @@ class BackdoorDetectImpl():
 
         acc_th, ano_th = 40.0, -1.5
 
-        dataset = 'cifar10_inv'
+        dataset = 'cifar10'
         num_of_epochs = 200
         dist_lst, acc_lst = [], []
         norm = 2
@@ -212,13 +488,15 @@ class BackdoorDetectImpl():
 
             size_input, size_last = (28, 28), 10
 
-        elif dataset == 'cifar10_inv' or dataset == 'cifar10_sem' or dataset == 'cifar10_tro':
+        elif dataset == 'cifar10_inv' or dataset == 'cifar10_sem' or dataset == 'cifar10_tro' or dataset=='cifar':
             if dataset == 'cifar10_inv':
                 file_name = './backdoor_models/cifar10_bd_inv.pt'
             elif dataset == 'cifar10_sem':
                 file_name = './backdoor_models/cifar10_bd_sem.pt'
             elif dataset == 'cifar10_tro':
                 file_name = './backdoor_models/cifar10_bd_tro.pt'
+            elif dataset == 'cifar':
+                file_name = './backdoor_models/cifar10_bd.pt'
 
             last_layer = 'fc2'
 
@@ -227,6 +505,28 @@ class BackdoorDetectImpl():
         
             sub_model = SubCIFAR10Net()
 
+            train_dataset = datasets.CIFAR10('./data', train=True, download=True, transform=transform)
+            test_dataset = datasets.CIFAR10('./data', train=False, transform=transform)
+
+            size_input, size_last = (3, 32, 32), 512
+        
+        elif dataset == 'cifar10':
+            file_name = './backdoor_models/cifar10_resnet18_bd.pt'
+            last_layer = 'avgpool'
+
+            def _resnet(arch, block, layers, pretrained, progress, device, **kwargs):
+                model = ResNet(block, layers, **kwargs)
+                if pretrained:
+                    state_dict = torch.load(
+                        file_name, map_location=device
+                    )
+                    model.load_state_dict(state_dict)
+                return model
+
+            model = _resnet("resnet18", BasicBlock, [2, 2, 2, 2], pretrained=True, progress=True, device="cpu")
+            model.avgpool.register_forward_hook(get_activation(last_layer))
+
+            sub_model = SubResNet(BasicBlock)
             train_dataset = datasets.CIFAR10('./data', train=True, download=True, transform=transform)
             test_dataset = datasets.CIFAR10('./data', train=False, transform=transform)
 
