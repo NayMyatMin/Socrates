5213cf2113ef05244f0cd9c21e2afdb79f18c181
NayMyatMin <naymyatmin97@gmail.com>
2023-02-22 08:51:53 +0000

    refactored

--- source/solver/backdoor_detect_impl_trojAI.py
+++ source/solver/backdoor_detect_impl_trojAI.py
@@ -1,40 +1,23 @@
-import torch
+import os
 import time
-import numpy as np
+import statistics
 import json
-
-from model.lib_models import *
-from model.lib_layers import *
-
-from poly_utils import *
-from solver.refinement_impl import Poly
-
-from utils import *
-from torch import nn
-from torch.utils.data import TensorDataset, DataLoader
-
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
-
+import numpy as np
+import torch
+import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
-from torch.optim.lr_scheduler import StepLR
+from torch.utils.data import DataLoader, TensorDataset
 
-import gurobipy as gp
-from gurobipy import GRB
-
-import math
-import ast
-
-from nn_utils import *
-
+from model.lib_models import *
+from model.lib_layers import *
 from wrn import WideResNet
-import time
-import statistics
+from nn_utils import *
+from solver.refinement_impl import Poly
+from utils import *
 
-###################################################### Troj-AI WRN CIFAR-10
+
 class SubWideResNet(nn.Module):
     def __init__(self, depth=40, num_classes=10, widen_factor=2, dropRate=0.0):
         super(SubWideResNet, self).__init__()
@@ -42,12 +25,12 @@ class SubWideResNet(nn.Module):
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(nChannels[3], num_classes)
         self.nChannels = nChannels[3]
-        
+
     def forward(self, x):
         out = torch.flatten(x, 1)
         return self.fc(out)
 
-###################################################### Troj-AI MNIST
+
 class SubMNISTNet(nn.Module):
     def __init__(self):
         super().__init__()
@@ -56,12 +39,24 @@ class SubMNISTNet(nn.Module):
     def forward(self, x):
         x = torch.flatten(x, 1)
         x = self.fc(x)
-        output = x # cross entropy in pytorch already includes softmax
+        output = x  # cross entropy in pytorch already includes softmax
         return output
-##################################################
 
-class BackdoorDetectImpl():
-    def __transfer_model(self, model, sub_model, dataset):
+
+class BackdoorDetectImpl:
+    def __init__(self):
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.train_kwargs = {'batch_size': 100}
+        self.test_kwargs = {'batch_size': 1000}
+        self.transform = transforms.ToTensor()
+        self.acc_th = 40.0
+        self.ano_th = -1.5
+        self.num_of_epochs = 100
+        self.norm = 2
+        self.size_input = None
+        self.size_last = None
+
+    def transfer_model(self, model, sub_model, dataset):
         params = model.named_parameters()
         sub_params = sub_model.named_parameters()
         dict_params = dict(sub_params)
@@ -76,60 +71,62 @@ class BackdoorDetectImpl():
                     dict_params['fc.weight'].data.copy_(param.data)
                 elif 'fc.bias' in name:
                     dict_params['fc.bias'].data.copy_(param.data)
-        
         sub_model.load_state_dict(dict_params)
 
-    def __generate_trigger(self, model, dataloader, num_of_epochs, size, target, norm, minx = None, maxx = None):
-        delta, eps, lamb = torch.zeros(size), 0.001, 1
+    def get_clamp(self, epoch, batch, max_clamp, min_clamp, num_batches, num_epochs):
+        t = epoch * num_batches + batch
+        T = num_batches * num_epochs
+        clamp = (max_clamp - min_clamp) * 0.5 * (1 + np.cos(np.pi * t / T)) + min_clamp
+        return clamp
 
-        for epoch in range(num_of_epochs):
+    def generate_trigger(self, model, dataloader, size, target, norm, minx, maxx=None):
+        delta = torch.zeros(size, requires_grad=True, device=next(model.parameters()).device)
+        eps = 0.001
+        lamb = 1
+        optimizer = torch.optim.Adam([delta], lr=0.01)
+
+        def adjust_learning_rate(optimizer, epoch, batch, num_batches):
+            """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+            lr = 0.01 * (0.1 ** (epoch // 30))
+            lr = lr * (1 - batch / num_batches)  # reduce learning rate linearly as we approach the end of the epoch
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr
+
+        for epoch in range(self.num_of_epochs):
             for batch, (x, y) in enumerate(dataloader):
-                delta.requires_grad = True
+                x = x.to(delta.device)
+                y = y.to(delta.device)
+                optimizer.zero_grad()
+                clamp = self.get_clamp(epoch, batch, max_clamp=10, min_clamp=1, num_batches=len(dataloader),
+                                    num_epochs=self.num_of_epochs)
                 x_adv = torch.clamp(torch.add(x, delta), minx, maxx)
-                target_tensor = torch.full(y.size(), target)
-
+                target_tensor = torch.full_like(y, target, device=next(model.parameters()).device, dtype=torch.long)
                 pred = model(x_adv)
                 loss = F.cross_entropy(pred, target_tensor) + lamb * torch.norm(delta, norm)
-
                 loss.backward()
-                
-                grad_data = delta.grad.data
-                delta = torch.clamp(delta - eps * grad_data.sign(), -10.0, 10.0).detach()
+                optimizer.step()
+                delta.data = torch.clamp(delta.data, -clamp, clamp)
+                adjust_learning_rate(optimizer, epoch, batch, len(dataloader))
+        return delta.detach().cpu()
 
-        return delta
-
-
-    def __check(self, model, dataloader, delta, target):
+    def check(self, model, dataloader, delta, target):
         size = len(dataloader.dataset)
         correct = 0
-
         for batch, (x, y) in enumerate(dataloader):
             x_adv = torch.clamp(torch.add(x, delta), 0.0)
             target_tensor = torch.full(y.size(), target)
-
             pred = model(x_adv)
-
             correct += (pred.argmax(1) == target_tensor).type(torch.int).sum().item()
-
         correct = correct / size * 100
         print('target = {}, test accuracy = {}'.format(target, correct))
-
         return correct
 
-
     def solve(self, model, assertion, display=None):
-        device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        train_kwargs, test_kwargs = {'batch_size': 100}, {'batch_size': 1000}
-        transform = transforms.ToTensor()
-
-        acc_th, ano_th = 40.0, -1.5
-        num_of_epochs = 100
-        norm = 2
-
-        root_dir = "benchmark-100"
+        root_dir = "benchmarking-cifar"
         for subdir, dirs, files in os.walk(root_dir):
             start_time = time.time()
             dist_lst, acc_lst = [], []
+
             for file in files:
                 if file == "model.pt":
                     model_path = os.path.join(subdir, file)
@@ -137,40 +134,32 @@ class BackdoorDetectImpl():
                     info_path = os.path.join(subdir, "info.json")
                     with open(info_path) as f:
                         info = json.load(f)
+
                         if info["dataset"] == "MNIST":
                             dataset = info["dataset"]
                             print('dataset =', dataset)
-                            last_layer = 'main[11]' # nn.BatchNorm1d(128)
-
+                            last_layer = 'main[11]'  # nn.BatchNorm1d(128)
                             model = load_model(MNIST_Network, model_path)
                             model.main[11].register_forward_hook(get_activation(last_layer))
-                        
                             sub_model = SubMNISTNet()
-                            
-                            train_dataset = datasets.MNIST('./data', train=True, download=False, transform=transform)
-                            test_dataset = datasets.MNIST('./data', train=False, transform=transform)
-
-                            size_input, size_last = (28, 28), 128
+                            train_dataset = datasets.MNIST('./data', train=True, download=False, transform=self.transform)
+                            test_dataset = datasets.MNIST('./data', train=False, transform=self.transform)
+                            self.size_input, self.size_last = (28, 28), 128
 
                         elif info["dataset"] == "CIFAR-10":
                             dataset = info["dataset"]
                             print('dataset =', dataset)
                             last_layer = 'avgpool'
-            
                             model = load_model(WideResNet, model_path)
                             model.avgpool.register_forward_hook(get_activation(last_layer))
-                        
                             sub_model = SubWideResNet(depth=40, num_classes=10, widen_factor=2, dropRate=0)
+                            train_dataset = datasets.CIFAR10('./data', train=True, download=False, transform=self.transform)
+                            test_dataset = datasets.CIFAR10('./data', train=False, transform=self.transform)
+                            self.size_input, self.size_last = (3, 32, 32), 128
 
-                            train_dataset = datasets.CIFAR10('./data', train=True, download=False, transform=transform)
-                            test_dataset = datasets.CIFAR10('./data', train=False, transform=transform)
-
-                            size_input, size_last = (3, 32, 32), 128
-
-                    self.__transfer_model(model, sub_model, dataset)
-
-                    train_dataloader = torch.utils.data.DataLoader(train_dataset, **train_kwargs)
-                    test_dataloader = torch.utils.data.DataLoader(test_dataset, **test_kwargs)
+                    self.transfer_model(model, sub_model, dataset)
+                    train_dataloader = DataLoader(train_dataset, **self.train_kwargs)
+                    test_dataloader = torch.utils.data.DataLoader(test_dataset, **self.test_kwargs)
 
                     last_layer_test_dataset = []
                     for batch, (x, y) in enumerate(test_dataloader):
@@ -179,55 +168,47 @@ class BackdoorDetectImpl():
                             last_layer_test_dataset.extend(F.relu(activation[last_layer]).detach().numpy())
                         elif dataset == 'CIFAR-10':
                             last_layer_test_dataset.extend(torch.flatten(activation[last_layer], 1).detach().numpy())
-
-                    last_layer_test_dataset = TensorDataset(torch.Tensor(np.array(last_layer_test_dataset)), torch.Tensor(np.array(test_dataset.targets))) # create dataset
-                    last_layer_test_dataloader = DataLoader(last_layer_test_dataset, **test_kwargs) # create dataloader
-
-                    # dataloader = test_dataloader
+                    last_layer_test_dataset = TensorDataset(torch.Tensor(np.array(last_layer_test_dataset)),
+                                                             torch.Tensor(np.array(test_dataset.targets)))
+                    last_layer_test_dataloader = DataLoader(last_layer_test_dataset, **self.test_kwargs)
                     dataloader = last_layer_test_dataloader
 
+                    print_line = '\n###############################\n'
+                    
                     for target in range(10):
-                        # delta = self.__generate_trigger(model, dataloader, num_of_epochs, size_input, target, norm, 0.0, 1.0)
-                        delta = self.__generate_trigger(sub_model, dataloader, num_of_epochs, size_last, target, norm, 0.0)
-                        delta = torch.where(abs(delta) < 0.1, delta - delta, delta)
+                        delta = self.generate_trigger(sub_model, dataloader, self.size_last, target, self.norm, minx=0.0)
+                        mask = abs(delta) < 0.1
+                        delta[mask] = 0
 
-                        print('\n###############################\n')
-
-                        # acc = self.__check(model, dataloader, delta, target)
-                        acc = self.__check(sub_model, dataloader, delta, target)
+                        print(print_line)
+                        acc = self.check(sub_model, dataloader, delta, target)
                         dist = torch.norm(delta, 0)
                         print('target = {}, delta = {}, dist = {}'.format(target, delta[:10], dist))
-
                         acc_lst.append(acc)
                         dist_lst.append(dist.detach().item())
 
-                    print('\n###############################\n')
-
                     acc_lst = np.array(acc_lst)
-                    print('acc_lst = {}'.format(acc_lst))
-
                     dist_lst = np.array(dist_lst)
-                    print('dist_lst = {}'.format(dist_lst))
-
-                    med = statistics.median(dist_lst)
-                    print('med = {}'.format(med))
-                    
+                    med = np.median(dist_lst)
                     dev_lst = abs(dist_lst - med)
-                    print('dev_lst = {}'.format(dev_lst))
-                    
-                    mad = statistics.median(dev_lst)
-                    print('mad = {}'.format(mad))
-                    
-                    ano_lst = (dist_lst - med) / mad
-                    print('ano_lst = {}'.format(ano_lst))
+                    mad = np.median(dev_lst)
+                    if mad > 0:
+                        ano_lst = (dist_lst - med) / mad
+                    else:
+                        ano_lst = dist_lst - med
 
                     end_time = time.time()
+
+                    print(print_line)
+                    print('acc_lst = {}'.format(acc_lst))
+                    print('dist_lst = {}'.format(dist_lst))
+                    print('med = {}'.format(med))                    
+                    print('dev_lst = {}'.format(dev_lst))                    
+                    print('mad = {}'.format(mad))                    
+                    print('ano_lst = {}'.format(ano_lst))                    
                     print("Elapsed time:", end_time - start_time, "seconds")
-                    print('###############################\n')
+                    print(print_line)
 
                     for target in range(10):
-                        if acc_lst[target] >= acc_th and ano_lst[target] <= ano_th:
+                        if acc_lst[target] >= self.acc_th and ano_lst[target] <= self.ano_th:
                             print('Detect backdoor at target = {}'.format(target))
-                    
-                    
-                    
