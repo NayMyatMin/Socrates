dfb515ff74c9ff76862471d852bd0db3aa9ffcf0
NayMyatMin <naymyatmin97@gmail.com>
2023-03-31 05:05:57 +0000

    Refactored

--- source/solver/backdoor_detect_impl_trojAI.py
+++ source/solver/backdoor_detect_impl_trojAI.py
@@ -1,12 +1,10 @@
 import os
 import time
-import statistics
 import json
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, TensorDataset
 
@@ -17,15 +15,11 @@ from nn_utils import *
 from solver.refinement_impl import Poly
 from utils import *
 
-from torchvision.utils import save_image
-
 class SubWideResNet(nn.Module):
     def __init__(self, depth=40, num_classes=10, widen_factor=2, dropRate=0.0):
         super(SubWideResNet, self).__init__()
         nChannels = [16, 16 * widen_factor, 32 * widen_factor, 64 * widen_factor]
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(nChannels[3], num_classes)
-        self.nChannels = nChannels[3]
 
     def forward(self, x):
         out = torch.flatten(x, 1)
@@ -39,9 +33,7 @@ class SubMNISTNet(nn.Module):
     def forward(self, x):
         x = torch.flatten(x, 1)
         x = self.fc(x)
-        output = x  # cross entropy in pytorch already includes softmax
-        return output
-
+        return x
 
 class BackdoorDetectImpl:
     def __init__(self):
@@ -81,10 +73,10 @@ class BackdoorDetectImpl:
         clamp = (max_clamp - min_clamp) * 0.5 * (1 + np.cos(np.pi * t / T)) + min_clamp
         return clamp
 
-    def generate_trigger(self, model, dataloader, size, target, norm, minx = None, maxx = None):
-        delta, eps, lamb = torch.rand(size, device=next(model.parameters()).device) * 0.1, 0.001, 1 #0.5 #torch.zero(size) 
+    def generate_trigger(self, model, dataloader, size, target, norm, minx=None, maxx=None):
+        delta, eps, lamb = torch.rand(size, device=next(model.parameters()).device) * 0.1, 0.001, 0.5
         best_delta, best_loss = None, float('inf')
-        clamp_values, trigger_quality, trigger_size = [], [], []
+        clamp_values, trigger_quality, trigger_size, trigger_distortion = [], [], [], []
         for epoch in range(self.num_of_epochs):
             for batch, (x, y) in enumerate(dataloader):
                 delta.requires_grad = True
@@ -96,8 +88,9 @@ class BackdoorDetectImpl:
                 trigger_quality.append(trigger_success / x.size(0))
                 trigger = x_adv - x
                 trigger_size.append(torch.norm(trigger.view(trigger.size(0), -1), norm, dim=1).mean().item())
+                trigger_distortion.append(torch.norm(delta, 2).item())
 
-                loss = F.cross_entropy(pred, target_tensor) + lamb * torch.norm(delta, norm)**2 # NOTE: try replace lamb * torch.norm(delta, norm) with torch.norm(delta, norm) ** 2 
+                loss = F.cross_entropy(pred, target_tensor) + lamb * torch.norm(delta, norm)
                 loss.backward()
                 grad_data = delta.grad.data
 
@@ -109,7 +102,7 @@ class BackdoorDetectImpl:
             if loss < best_loss:
                 best_delta, best_loss = delta.detach(), loss
 
-        return best_delta
+        return best_delta, np.mean(trigger_size), np.mean(trigger_distortion)
 
     def check(self, model, dataloader, delta, target):
         size = len(dataloader.dataset)
@@ -121,11 +114,91 @@ class BackdoorDetectImpl:
                 pred = model(x_adv)
             correct += (pred.argmax(1) == target_tensor).type(torch.int).sum().item()
         correct = correct / size * 100
-        print('target = {}, test accuracy = {}'.format(target, correct))
+        print('target = {}, attack success rate = {}'.format(target, correct))
         return correct
+    
+    def load_and_prepare_model(self, model_path):
+        with open(os.path.join(os.path.dirname(model_path), "info.json")) as f:
+            info = json.load(f)
+
+            if info["dataset"] == "MNIST":
+                dataset = info["dataset"]
+                last_layer = 'main[11]'  # nn.BatchNorm1d(128)
+                model = load_model(MNIST_Network, model_path)
+                model.main[11].register_forward_hook(get_activation(last_layer))
+                sub_model = SubMNISTNet()
+                train_dataset = datasets.MNIST('./data', train=True, download=False, transform=self.transform)
+                test_dataset = datasets.MNIST('./data', train=False, transform=self.transform)
+                self.size_input, self.size_last = (28, 28), 128
+
+            elif info["dataset"] == "CIFAR-10":
+                dataset = info["dataset"]
+                last_layer = 'avgpool'
+                model = load_model(WideResNet, model_path)
+                model.avgpool.register_forward_hook(get_activation(last_layer))
+                sub_model = SubWideResNet(depth=40, num_classes=10, widen_factor=2, dropRate=0)
+                train_dataset = datasets.CIFAR10('./data', train=True, download=False, transform=self.transform)
+                test_dataset = datasets.CIFAR10('./data', train=False, transform=self.transform)
+                self.size_input, self.size_last = (3, 32, 32), 128
+
+        self.transfer_model(model, sub_model, dataset)
+        return model, sub_model, dataset, train_dataset, test_dataset, last_layer
+
+    def get_last_layer_activations(self, model, test_dataset, last_layer, dataset):
+        test_dataloader = torch.utils.data.DataLoader(test_dataset, **self.test_kwargs)
+        last_layer_test_dataset = []
+        for batch, (x, y) in enumerate(test_dataloader):
+            model(x)
+            if dataset == 'MNIST':
+                last_layer_test_dataset.extend(F.relu(activation[last_layer]).detach().numpy())
+            elif dataset == 'CIFAR-10':
+                last_layer_test_dataset.extend(torch.flatten(activation[last_layer], 1).detach().numpy())
+        last_layer_test_dataset = TensorDataset(torch.Tensor(np.array(last_layer_test_dataset)),
+                                                torch.Tensor(np.array(test_dataset.targets)))
+        last_layer_test_dataloader = DataLoader(last_layer_test_dataset, **self.test_kwargs)
+        return last_layer_test_dataloader
+
+    def evaluate_triggers(self, model, sub_model, dataloader):
+        acc_lst, dist0_lst, dist2_lst = [], [], []
+
+        for target in range(10):
+            delta, trigger_size, trigger_distortion = self.generate_trigger(sub_model, dataloader, self.size_last, target, self.norm, minx=0.0, maxx=None)
+            delta = torch.where(abs(delta) < 0.1, delta - delta, delta)
+            acc = self.check(sub_model, dataloader, delta, target)
+            dist0 = torch.norm(delta, 0)
+            dist2 = torch.norm(delta, 2)
+            acc_lst.append(acc)
+            dist0_lst.append(dist0.detach().item())
+            dist2_lst.append(dist2.detach().item())
+            print('target = {}, delta = {}, dist0 = {}, dist2 = {}'.format(target, delta[:10], dist0, dist2))
+        return acc_lst, dist0_lst, dist2_lst
+
+    def detect_backdoors(self, acc_lst, dist0_lst, dist2_lst):
+        detected_backdoors = []
+        med = np.median(dist0_lst)
+        dev_lst = abs(np.array(dist0_lst) - med)
+        mad = np.median(dev_lst)
+        ano_lst = (np.array(dist0_lst) - med) / mad
+
+        print('acc_lst = {}'.format(acc_lst))
+        print('dist0_lst = {}'.format(dist0_lst)) 
+        print('dist2_lst = {}'.format(dist2_lst))
+        print('med = {}'.format(med))                    
+        print('dev_lst = {}'.format(dev_lst))                    
+        print('mad = {}'.format(mad))                    
+        print('ano_lst = {}'.format(ano_lst))                   
+
+        for target in range(10):
+            if acc_lst[target] >= self.acc_th and (ano_lst[target] <= self.ano_th or dist0_lst[target] <= self.d0_th or dist2_lst[target] <= self.d2_th):
+                detected_backdoors.append(target)
+
+        return detected_backdoors
 
     def solve(self, model, assertion, display=None):
+        start_time = time.time()
+
         root_dir = "benchmark-100"
+        separator = f"\n{'*' * 100}\n"
         for subdir, dirs, files in os.walk(root_dir):
             start_time = time.time()
             dist0_lst, dist2_lst, acc_lst = [], [], []
@@ -134,83 +207,20 @@ class BackdoorDetectImpl:
                 if file == "model.pt":
                     model_path = os.path.join(subdir, file)
                     print("Model Path:", model_path)
-                    info_path = os.path.join(subdir, "info.json")
-                    with open(info_path) as f:
-                        info = json.load(f)
 
-                        if info["dataset"] == "MNIST":
-                            dataset = info["dataset"]
-                            print('dataset =', dataset)
-                            last_layer = 'main[11]'  # nn.BatchNorm1d(128)
-                            model = load_model(MNIST_Network, model_path)
-                            model.main[11].register_forward_hook(get_activation(last_layer))
-                            sub_model = SubMNISTNet()
-                            train_dataset = datasets.MNIST('./data', train=True, download=False, transform=self.transform)
-                            test_dataset = datasets.MNIST('./data', train=False, transform=self.transform)
-                            self.size_input, self.size_last = (28, 28), 128
-
-                        elif info["dataset"] == "CIFAR-10":
-                            dataset = info["dataset"]
-                            print('dataset =', dataset)
-                            last_layer = 'avgpool'
-                            model = load_model(WideResNet, model_path)
-                            model.avgpool.register_forward_hook(get_activation(last_layer))
-                            sub_model = SubWideResNet(depth=40, num_classes=10, widen_factor=2, dropRate=0)
-                            train_dataset = datasets.CIFAR10('./data', train=True, download=False, transform=self.transform)
-                            test_dataset = datasets.CIFAR10('./data', train=False, transform=self.transform)
-                            self.size_input, self.size_last = (3, 32, 32), 128
-
-                    self.transfer_model(model, sub_model, dataset)
-                    train_dataloader = DataLoader(train_dataset, **self.train_kwargs)
-                    test_dataloader = torch.utils.data.DataLoader(test_dataset, **self.test_kwargs)
-
-                    last_layer_test_dataset = []
-                    for batch, (x, y) in enumerate(test_dataloader):
-                        model(x)
-                        if dataset == 'MNIST':
-                            last_layer_test_dataset.extend(F.relu(activation[last_layer]).detach().numpy())
-                        elif dataset == 'CIFAR-10':
-                            last_layer_test_dataset.extend(torch.flatten(activation[last_layer], 1).detach().numpy())
-                    last_layer_test_dataset = TensorDataset(torch.Tensor(np.array(last_layer_test_dataset)),
-                                                             torch.Tensor(np.array(test_dataset.targets)))
-                    last_layer_test_dataloader = DataLoader(last_layer_test_dataset, **self.test_kwargs)
-                    dataloader = last_layer_test_dataloader
-
-                    print_line = '\n###############################\n'
+                    model, sub_model, dataset, train_dataset, test_dataset, last_layer = self.load_and_prepare_model(model_path)
+                    dataloader = self.get_last_layer_activations(model, test_dataset, last_layer, dataset)
                     
-                    for target in range(10):
-                        delta = self.generate_trigger(sub_model, dataloader, self.size_last, target, self.norm, minx=0.0, maxx=None)
-                        delta = torch.where(abs(delta) < 0.1, delta - delta, delta)
-
-                        print(print_line)
-                        acc = self.check(sub_model, dataloader, delta, target)
-                        dist0 = torch.norm(delta, 0)
-                        dist2 = torch.norm(delta, 2)
-                        print('target = {}, delta = {}, dist0 = {}, dist2 = {}'.format(target, delta[:10], dist0, dist2))
-                        acc_lst.append(acc)
-                        dist0_lst.append(dist0.detach().item())
-                        dist2_lst.append(dist2.detach().item())
-
-                    acc_lst = np.array(acc_lst)
-                    dist0_lst = np.array(dist0_lst)
-                    dist2_lst = np.array(dist2_lst)
-                    med = np.median(dist0_lst)
-                    dev_lst = abs(dist0_lst - med)
-                    mad = np.median(dev_lst)
-                    ano_lst = (dist0_lst - med) / mad
+                    print(separator)
+                    acc_lst, dist0_lst, dist2_lst = self.evaluate_triggers(model, sub_model, dataloader)
+                    print(separator)
+                    detected_backdoors = self.detect_backdoors(acc_lst, dist0_lst, dist2_lst)
+                    print(separator)
                     end_time = time.time()
-
-                    print(print_line)
-                    print('acc_lst = {}'.format(acc_lst))
-                    print('dist0_lst = {}'.format(dist0_lst)) # NOTE: dist_lst here seems to be 0-norm distance already
-                    print('dist2_lst = {}'.format(dist2_lst))
-                    print('med = {}'.format(med))                    
-                    print('dev_lst = {}'.format(dev_lst))                    
-                    print('mad = {}'.format(mad))                    
-                    print('ano_lst = {}'.format(ano_lst))                    
+                    
+                    if detected_backdoors:
+                        print("Detected backdoors at targets:", detected_backdoors)
+                    else:
+                        print("No backdoors detected.")
                     print("Elapsed time:", end_time - start_time, "seconds")
-                    print(print_line)
-
-                    for target in range(10):
-                        if acc_lst[target] >= self.acc_th and (ano_lst[target] <= self.ano_th or dist0_lst[target] <= self.d0_th or dist2_lst[target] <= self.d2_th):
-                            print('Detect backdoor at target = {}'.format(target))
+                    print(separator)
